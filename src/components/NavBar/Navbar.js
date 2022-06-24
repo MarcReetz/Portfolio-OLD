b13c5208ea3742f57cf7284ad867fa3f56e88700
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2022-06-24 18:04:31 +0200

    add icons and changes to Navbar

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,8 +1,8 @@
-
+import "./Navbar.css"
 export default function Navbar () {
   return (
     <header>
-      <h4>PLACE LOGO HEAR</h4> <h4>ICON</h4>
+      <h4>PLACE LOGO HEAR</h4> <button class="menu"></button>
     </header>
   )
 }
