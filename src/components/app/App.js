e31b588b6514bf56faa1d6bb086bb0111f5ce18b
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2022-06-26 01:04:00 +0200

    add nav mobile

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import Intro from "../../features/Intro/Intro";
 import Navbar from "../NavBar/Navbar";
+import { useState } from "react";
+import NavbarExtended from "../NavBar/NavbarExtended";
 
 function App() {
+
+  const [NavOpen,setNavOpen] = useState(false);
+
+  const toogleNav = () => {
+    setNavOpen(NavOpen ? false : true)
+  };
+
   return (
     <div>
-      <Navbar />
+      <Navbar navOpen= {NavOpen} onClick= {toogleNav}/>
+      { NavOpen && <NavbarExtended onClick={toogleNav}/>}
       <main className="App">
         <Intro />
       </main>
